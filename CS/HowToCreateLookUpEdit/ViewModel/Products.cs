62c5f7a9c936575baa2f9c3de75ec25138bc56f7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToCreateLookUpEdit/ViewModel/Products.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using DevExpress.Mvvm;
+
+namespace HowToCreateLookUpEdit.ViewModel
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ VB/HowToCreateLookUpEdit/MainWindow.xaml.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Grid.LookUp
+Imports HowToCreateLookUpEdit.ViewModel
+
+Namespace HowToCreateLookUpEdit
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
