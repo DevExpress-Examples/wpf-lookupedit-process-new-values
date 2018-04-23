c32b5ab90476922f62e25fceddb5123b304a22b4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/HowToCreateLookUpEdit/ViewModel/Products.vb
@@ -0,0 +1,63 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports DevExpress.Xpf.Mvvm
+
+Namespace HowToCreateLookUpEdit.ViewModel
+	Public Class Product
+		Private privateID As Integer
+		Public Property ID() As Integer
+			Get
+				Return privateID
+			End Get
+			Set(ByVal value As Integer)
+				privateID = value
+			End Set
+		End Property
+		Private privateProductName As String
+		Public Property ProductName() As String
+			Get
+				Return privateProductName
+			End Get
+			Set(ByVal value As String)
+				privateProductName = value
+			End Set
+		End Property
+		Private privateCountry As String
+		Public Property Country() As String
+			Get
+				Return privateCountry
+			End Get
+			Set(ByVal value As String)
+				privateCountry = value
+			End Set
+		End Property
+		Private privateCity As String
+		Public Property City() As String
+			Get
+				Return privateCity
+			End Get
+			Set(ByVal value As String)
+				privateCity = value
+			End Set
+		End Property
+		Private privateUnitPrice As Integer
+		Public Property UnitPrice() As Integer
+			Get
+				Return privateUnitPrice
+			End Get
+			Set(ByVal value As Integer)
+				privateUnitPrice = value
+			End Set
+		End Property
+		Private privateQuantity As Integer
+		Public Property Quantity() As Integer
+			Get
+				Return privateQuantity
+			End Get
+			Set(ByVal value As Integer)
+				privateQuantity = value
+			End Set
+		End Property
+	End Class
+End Namespace
